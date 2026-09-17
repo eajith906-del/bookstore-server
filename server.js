f4a17568e62b308382c12d339001ddc4f004de37
Ajith303 <149502704+Ajith303@users.noreply.github.com>
2026-09-17 11:13:20 +0530

    Fix CORS for Vercel frontend

--- server.js
+++ server.js
@@ -3,41 +3,66 @@ const app = express()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
-const cors = require ("cors")
-const cookieParser = require("cookie-parser")   
-require('dotenv').config();
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
+require("dotenv").config();
+
 const port = process.env.PORT
 
-const userRoute = require ("./route/userRoute")
-const contactRoute = require ("./route/contactRoute")
-const productRoute = require ("./route/productRoute")
+const userRoute = require("./route/userRoute")
+const contactRoute = require("./route/contactRoute")
+const productRoute = require("./route/productRoute")
 const orderRoute = require("./route/orderRoute");
-// const AuthMiddleWare = require ("./MiddleWare/AuthMiddlleWare")
+// const AuthMiddleWare = require("./MiddleWare/AuthMiddlleWare")
 
+
+// CORS
 const corsOptions = {
-     origin:"http://localhost:3000",
-     credentials:true,
-     optionSuccessStatus:200 
+    origin: function (origin, callback) {
+
+        const allowedOrigins = [
+            "http://localhost:3000",
+            "https://book-store-peach-five.vercel.app"
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+
+    credentials: true,
+    optionSuccessStatus: 200
 }
 
-app.use(cookieParser())         
+
+app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(morgan("dev"))
-app.use(cors(corsOptions))  
-// app.use(AuthMiddleWare)           
+app.use(cors(corsOptions))
+// app.use(AuthMiddleWare)
 
-app.use("/user",userRoute)
-app.use("/product",productRoute)
-app.use("/contact",contactRoute)
+
+app.use("/user", userRoute)
+app.use("/product", productRoute)
+app.use("/contact", contactRoute)
 app.use("/order", orderRoute);
-app.get('/', (req, res) => {
+
+
+app.get("/", (req, res) => {
     console.log("Root route hit");
     res.send("API is running 🚀");
 });
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("mongoose connected"))
-.catch(err=>console.log(err))
 
-app.listen(port,()=>{console.log(`connected in ${port}`)})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("mongoose connected"))
+    .catch(err => console.log(err))
+
+
+app.listen(port, () => {
+    console.log(`connected in ${port}`)
+})
